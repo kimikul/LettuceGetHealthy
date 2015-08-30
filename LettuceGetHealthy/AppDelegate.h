7f9286a6c2f2b61ec034c22720bbac099c40d36b
Kimberly Hsiao <kimberly.hsiao@gmail.com>
2015-08-29 23:50:15 -0700

    Initial Commit

---
+++ LettuceGetHealthy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LettuceGetHealthy
+//
+//  Created by Kimberly Hsiao on 8/29/15.
+//  Copyright (c) 2015 hsiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LettuceGetHealthy/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LettuceGetHealthy
+//
+//  Created by Kimberly Hsiao on 8/29/15.
+//  Copyright (c) 2015 hsiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
